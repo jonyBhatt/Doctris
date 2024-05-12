2bb19f28f6c15460c3d61026885f5c7c6835d370
Jony <jonybhattacharjee886@gmail.com>
2024-05-13 01:04:46 +0600

    fix signup

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,11 +1,15 @@
 import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/prisma";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const { firstname, lastname, email, password, role } = body;
-  const genSalt = await bcrypt.genSalt(10)
-  const hasPass = await bcrypt.hash(password, genSalt)
+
+  if (!firstname || !lastname || !email || password)
+    return Response.json({ messageL: "Invalid fields" }, { status: 403 });
+
+  const genSalt = await bcrypt.genSalt(10);
+  const hasPass = await bcrypt.hash(password, genSalt);
 
   try {
     const newUser = await prisma.user.create({
